3f65b309bce67fa96347fd09471594cf85c30f6b
Daniel Yu <charlesyou1234567@gmail.com>
2022-11-24 23:44:00 +0800

    shengi

--- js/config.js
+++ js/config.js
@@ -1,25 +1,25 @@
 
 function loadingPageAnimaiton(){
     let g = gsap.timeline()
-    g.fromTo(".loading-page .rotate .g2",{
+    g.fromTo(".loading-page .rotate .g2 ",{
         opacity:0
     },{
         opacity:1,
-        duration:0.8,
+        duration:1.32,
    
     }).fromTo(".loading-page .rotate .g1",{
         opacity:0
     },{
         opacity:1,
-        duration:0.8,
+        duration:1.32,
    
-    },"<+0.4").fromTo(".loading-page .rotate .g3",{
+    },"<+0.6").fromTo(".loading-page .rotate .g3",{
         opacity:0
     },{
         opacity:1,
-        duration:0.8,
+        duration:1.32,
    
-    },"<+0.4")
+    },"<+0.6")
     let gg = gsap.timeline()
     gg.fromTo(".loading-page .g1 .circle1",{
         strokeDasharray: 1000,
@@ -28,7 +28,8 @@ function loadingPageAnimaiton(){
      
         strokeDashoffset: 1000,
         duration:1.5,
-        ease: "none"
+        ease: "none",
+        delay:0.5
     }).fromTo(".loading-page .g1 .circle2",{
         strokeDasharray: 1000,
         strokeDashoffset: 100
@@ -81,7 +82,8 @@ function loadingPageAnimaiton(){
      
         strokeDashoffset: 1000,
         duration:1.5,
-        ease: "none"
+        ease: "none",
+        delay:0.5
     },"<+0.7").fromTo(".loading-page .g2 .circle2",{
         strokeDasharray: 1000,
         strokeDashoffset: 100
@@ -99,9 +101,7 @@ function loadingPageAnimaiton(){
         ease: "none"
     },"<+0.1").fromTo(".loading-page .g2 .circle1",{
         strokeDasharray: 1000,
- 
     },{
-     
         strokeDashoffset: 230,
         duration:1.5,
         ease: "none"
@@ -135,7 +135,8 @@ function loadingPageAnimaiton(){
      
         strokeDashoffset: 1000,
         duration:1.5,
-        ease: "none"
+        ease: "none",
+        delay:0.5
     },"<+0.3").fromTo(".loading-page .g3 .circle2",{
         strokeDasharray: 1000,
         strokeDashoffset: 100
@@ -185,19 +186,19 @@ function loadingPageAnimaiton(){
         {
             x:"-50%",
             duration:0.8,
+        },"<").to(".rotate",
+        {
+          
+           rotate:90,
+           scale:0.2,
+           transformOrigin:"center",
+            duration:0.8,
+           x:"-1.7%"
         },"<").to(".g1",
         {
             opacity:0,
             duartion:0.01
-        },">").to(".rotate",
-         {
-           
-            rotate:90,
-            scale:0.2,
-            transformOrigin:"center",
-             duration:0.8,
-            x:"-1.7%"
-         },"<").fromTo(".loading-page .g2 .circle1",{
+        },">").fromTo(".loading-page .g2 .circle1",{
             strokeDasharray: 1000,
             strokeDashoffset:230,
         },{
@@ -274,10 +275,28 @@ function loadingPageAnimaiton(){
             opacity:1,
             duration:0.8
          },"<+0.5").to(".loading-page",{
-            opacity:0,
+            backgroundColor:"transparent",
             duration:2,
+         }).to(".loading-page .contentBox",{
+            y:"-14vw",
+            duration:1.5,
+         },"<+0.2").to(".rotate",
+         {
+           
+            rotate:0,
+            scaleY:0.23,
+            scaleX:0.2,
+            transformOrigin:"center",
+             duration:0.8,
+         
+         },"<").to(".loading-page .titleBox",
+         {
 
-         })
+            scale:0.5,
+
+             duration:0.8,
+         
+         },"<")
     })
 
     
@@ -285,29 +304,13 @@ function loadingPageAnimaiton(){
 }
 loadingPageAnimaiton()
 
-//menu的下拉式選單
-function selectMenu(event){
-    $('.sub').each((i,item)=>{
-        if(event.target == item){
-            $('.sub').eq(i).toggleClass('active');/*新增class*/
-            $('.sub-open').eq(i).slideToggle(); /*下拉選單被展開*/
-        }
-    })
-   
-  
-}
-$('.sub').click(function(event) {
-    event.preventDefault();
-    selectMenu(event)
-});
-
-//menu的下拉式選單
 
 
-//menu開啟關閉
+
+//menu開啟關閉與模糊
    
     $(".close").click(()=>{
-        $(".menu").css("right","-20%")
+        $(".menu").css("right","-30%")
         $(".index").css({
             filter:"blur(0px)"
         })
@@ -319,4 +322,32 @@ $('.sub').click(function(event) {
             filter:"blur(5px)"
         })
     })
-//Menu開啟關閉
+//Menu開啟關閉模糊
+
+//Menu點點浮動
+    $(".menu-button").mousemove((e)=>{
+        let gg = gsap.timeline()
+        let dotRange = $(".menu-button :nth-child(1)").offset().left
+        let dotRange2 = $(".menu-button :nth-child(1)").offset().top
+    
+        gg.to(".menu-button :nth-child(1)",{
+            x:e.pageX-dotRange-30,
+            y:e.pageY-dotRange2-40,
+            duration:0.5,
+            overwrite:"auto",
+            ease:"power1"
+        })
+    })
+    $(".menu-button").mouseleave((e)=>{
+        let gg = gsap.timeline()
+    
+
+        gg.to(".menu-button :nth-child(1)",{
+            x:0,
+            y:0,
+            duration:0.5,
+            overwrite:"auto",
+            ease:"power1"
+        })
+    })
+//Menu點點浮動
