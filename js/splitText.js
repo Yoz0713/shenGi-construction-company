5d8cd17f31eba32015d8022b35cc2b63f3483bf1
Danile Yu <charlesyou1234567@gmail.com>
2023-02-07 12:21:11 +0800

    animation done

--- js/splitText.js
+++ js/splitText.js
@@ -1,33 +1,36 @@
 function splitText(wrap) {
 
-    wrap = document.querySelector(`${wrap}`);
-    text = wrap.innerText
+    wrap = document.querySelectorAll(`${wrap}`);
+    wrap.forEach((item, i) => {
+        text = item.innerText
 
-    wrap.innerHTML = null
-    let str = `${text}`;
-    let brExp = /\n/i;
-    let arrElement = [];
-    let arr = str.split(brExp)
-    arr.forEach((item, i) => {
-        item = item.split("");
+        item.innerHTML = null
+        let str = `${text}`;
+        let brExp = /\n/i;
+        let arrElement = [];
+        let arr = str.split(brExp)
+        arr.forEach((item) => {
+            item = item.split("");
 
-        let a = item.map((item2, i) => {
+            let a = item.map((item2, i) => {
 
-            if (item2 == " ") {
-                item2 = `<span class="blind"><span>\xa0</span></span>`
-                return item2
-            } else {
-                item2 = `<span class="blind"><span>${item2}</span></span>`
-                return item2
-            }
+                if (item2 == " ") {
+                    item2 = `<span class="blind"><span>\xa0</span></span>`
+                    return item2
+                } else {
+                    item2 = `<span class="blind"><span>${item2}</span></span>`
+                    return item2
+                }
+            })
+            arrElement.push(a)
         })
-        arrElement.push(a)
-    })
-    let finalStr = arrElement.map((item) => {
-        return item.join("")
+        let finalStr = arrElement.map((item) => {
+            return item.join("")
 
+        })
+
+        item.innerHTML = finalStr.join("<br/>");
     })
 
-    wrap.innerHTML = finalStr.join("<br/>");
 
 }
